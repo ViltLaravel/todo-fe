4c5810cd5e0b2cb8f8efb6d1f02095bfc8ed8741
ViltLaravel <nicoleamoguis15@gmail.com>
2024-05-13 15:43:14 +0800

    @fix: layout

--- src/component/Header.vue
+++ src/component/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sm:w-[500px] md:w-[700px] sm:h-[50px] sm:py-[10px] font-mono text-[18px] font-[500] text-gray-300 rounded-lg bg-[#7469B6] text-center mb-4">
+    <div class="w-[400px] sm:w-[500px] md:w-[700px] sm:h-[50px] sm:py-[10px] font-mono text-[18px] font-[500] text-gray-300 rounded-lg bg-[#7469B6] text-center mb-4">
         <h4>TASK MANAGEMENT</h4>
     </div>
 </template>
